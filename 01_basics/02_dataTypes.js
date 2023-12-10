5f776383fe98aed95c89cc88126b6e5200f3d01a
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-12-10 21:45:08 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) // we are using node.js not browser
+
+let name="Manjot"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standolone value
+// undefined =>
+// symbol => unique (use to find unique values)
+
+// interview question  null is an Object ;
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
