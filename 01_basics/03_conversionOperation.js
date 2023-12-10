e7d925cfcfb8360e847459b94ba0588c886058f8
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-12-10 22:17:01 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score= "33"
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+// let isLogged = "M";
+
+// let booleanIsLoggedIn = Boolean (isLogged);
+// console.log(booleanIsLoggedIn);
+
+// 1,2,3,4,..... =>  true
+// 0 => false
+// "" => false
+// "mafaf...." => true
+
+
+
+// ******************************* Operations **********************************//
+
+
+let value =3
+let negValue = -value
+
+// console.leg(negValue);
+
+// "3" + 3 +1=> 331    if first string then treat as string
+// 3+1+"3" => 43       if first not string then calculate at string then merge
+
