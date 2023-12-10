f49083a43d703f13e95e19c07c5d63badab4bbbd
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-12-10 22:17:10 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log( 2 > 1);
+// console.log( 2 >= 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+//always use === instead of == it can also check data types
+
